113d11dd2883f6d6d97b70ef53ad60303abd58c5
lzn1234 <1157579266@qq.com>
2020-10-13 17:08:55 +0800

    version1

---
+++ app/src/main/java/com/example/myapplication/Lab_2_1.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+
+public class Lab_2_1 extends AppCompatActivity {
+//   更新后
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab_2_1);
+
+    }
+}
